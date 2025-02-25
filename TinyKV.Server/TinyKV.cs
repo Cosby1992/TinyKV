09351d309a9d3f84ed7c51122e5d1494d95bb644
cosby1992 <andersbirkedahl@hotmail.com>
2025-02-25 20:34:47 +0100

    refactor: small refactor

--- TinyKV.Server/TinyKV.cs
+++ TinyKV.Server/TinyKV.cs
@@ -4,22 +4,9 @@ using Microsoft.Extensions.Caching.Memory;
 namespace TinyKV.Server;
 public class Store(IMemoryCache cache) : Hub
 {
-    public string? Get(string key)
-    { 
-        cache.TryGetValue(key, out string? value);
-        return value;
-    }
-
-    public void Set(string key, string value)
-    {
-        _ = cache.Set(key, value);
-    }
-
-    public bool Has(string key)
-    {
-        return cache.TryGetValue(key, out _);
-    }
-
+    public string? Get(string key) => cache.TryGetValue(key, out string? value) ? value : null;
+    public void Set(string key, string value) => _ = cache.Set(key, value);
+    public bool Has(string key) => cache.TryGetValue(key, out _);
     public bool Delete(string key)
     {
         cache.Remove(key);
