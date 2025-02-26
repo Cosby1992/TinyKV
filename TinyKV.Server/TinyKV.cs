43d805d5fd08ccd873acffa23296fe407645cb62
cosby1992 <andersbirkedahl@hotmail.com>
2025-02-26 20:03:28 +0100

    feat: added batch functions to client previous commit should have contained this change

--- TinyKV.Server/TinyKV.cs
+++ TinyKV.Server/TinyKV.cs
@@ -5,14 +5,55 @@ namespace TinyKV.Server;
 public class KVMemStore() : Hub
 {
     private static readonly ConcurrentDictionary<string, string> _store = new(Environment.ProcessorCount * 2, 200_000);
-    
+
     public Task<string?> Get(string key) => Task.FromResult(_store.TryGetValue(key, out string? value) ? value : null);
     public Task Set(string key, string value) => Task.FromResult(_ = _store[key] = value);
     public Task<bool> Has(string key) => Task.FromResult(_store.TryGetValue(key, out _));
     public Task<bool> Delete(string key) => Task.FromResult(_store.TryRemove(key, out _));
 
-    public Task Clear() {
+    public Task Clear()
+    {
         _store.Clear();
         return Task.CompletedTask;
+    }
+
+    // Batch functions
+    public Task SetBatch(Dictionary<string, string> keyValues)
+    {
+        foreach (var kv in keyValues)
+        {
+            _store[kv.Key] = kv.Value;
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task<Dictionary<string, string>> GetBatch(List<string> keys)
+    {
+        var results = new Dictionary<string, string>();
+        foreach (var key in keys)
+        {
+            if (_store.TryGetValue(key, out var value))
+            {
+                results[key] = value;
+            }
+        }
+        return Task.FromResult(results);
+    }
+
+    public Task<bool[]> HasBatch(List<string> keys)
+    {
+        var results = keys.Select(key => _store.ContainsKey(key)).ToArray();
+        return Task.FromResult(results);
+    }
+
+    public Task<int> DeleteBatch(List<string> keys)
+    {
+        int deletedCount = 0;
+        foreach (var key in keys)
+        {
+            if (_store.TryRemove(key, out _))
+                deletedCount++;
+        }
+        return Task.FromResult(deletedCount);
     }
 }
