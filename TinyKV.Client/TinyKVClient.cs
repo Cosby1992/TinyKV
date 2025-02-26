d368de7f570ba4a7561c722cb445114adb87732f
cosby1992 <andersbirkedahl@hotmail.com>
2025-02-26 20:02:25 +0100

    feat: added batch functions to server and client

--- TinyKV.Client/TinyKVClient.cs
+++ TinyKV.Client/TinyKVClient.cs
@@ -9,9 +9,24 @@ public class TinyKVClient(string serverUrl)
             .Build();
 
     public async Task StartAsync() => await _connection.StartAsync();
+
+    // Single Operations
     public async Task SetAsync(string key, string value) => await _connection.InvokeAsync("Set", key, value);
     public async Task<string> GetAsync(string key) => await _connection.InvokeAsync<string>("Get", key);
     public async Task<bool> HasAsync(string key) => await _connection.InvokeAsync<bool>("Has", key);
     public async Task<bool> DeleteAsync(string key) => await _connection.InvokeAsync<bool>("Delete", key);
     public async Task ClearAsync(string key) => await _connection.InvokeAsync<bool>("Clear", key);
+
+    // Batch Operations
+    public async Task SetBatchAsync(Dictionary<string, string> keyValues) =>
+        await _connection.InvokeAsync("SetBatch", keyValues);
+
+    public async Task<Dictionary<string, string>> GetBatchAsync(List<string> keys) =>
+        await _connection.InvokeAsync<Dictionary<string, string>>("GetBatch", keys);
+
+    public async Task<bool[]> HasBatchAsync(List<string> keys) =>
+        await _connection.InvokeAsync<bool[]>("HasBatch", keys);
+
+    public async Task<int> DeleteBatchAsync(List<string> keys) =>
+        await _connection.InvokeAsync<int>("DeleteBatch", keys);
 }
--- TinyKV.Server/Program.cs
+++ TinyKV.Server/Program.cs
@@ -2,7 +2,10 @@ using TinyKV.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMemoryCache();
-builder.Services.AddSignalR();
+builder.Services.AddSignalR(options =>
+{
+    options.MaximumReceiveMessageSize = 1024 * 1024; // Increase to 1MB (default is 32KB)
+});
 
 var app = builder.Build();
 
